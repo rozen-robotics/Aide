d9d1811b45cb78eb0e83b136740b5387c1ecf406
mikhail shafiev <m_shafiev@list.ru>
2024-09-22 14:09:43 +0300

    Revert "update voice input"
    This reverts commit 4a0ef086d9740f8a79da294a2201b23f890bbabd.

--- robot/urls.py
+++ robot/urls.py
@@ -10,6 +10,4 @@ urlpatterns = [
     path('process_question_ajax/', views.process_question_ajax, name='process_question_ajax'),
     path('recognize_face_cords_ajax/', views.recognize_face_cords_ajax, name='recognize_face_cords_ajax'),
 
-    path('process_audio_ajax/', views.process_audio_ajax, name='process_audio_ajax'),
-
 ]
